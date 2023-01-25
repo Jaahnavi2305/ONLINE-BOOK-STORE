6d88f4b61c323189114e110c6df3498eadb5acb9
Pasula <akhila.pasula@capgemini.com>
2023-01-25 10:02:43 +0530

    newcommitadded

--- src/main/java/com/bootapp/rest/restapp/OnlineBookStoreAppApplication.java
+++ src/main/java/com/bootapp/rest/restapp/OnlineBookStoreAppApplication.java
@@ -2,6 +2,7 @@ package com.bootapp.rest.restapp;
 
 import org.springframework.boot.SpringApplication;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
@@ -12,3 +13,5 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 	}
 
 }
+
+
--- src/main/java/com/bootapp/rest/restapp/controller/ReviewController.java
+++ src/main/java/com/bootapp/rest/restapp/controller/ReviewController.java
@@ -2,6 +2,8 @@ package com.bootapp.rest.restapp.controller;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +41,7 @@ public class ReviewController {
 
 	/* Review PUT API */
 	@PutMapping("/update/{reviewId}")
-	public ResponseEntity<String> updateReviewById(@PathVariable("reviewId") int authorId, @RequestBody Review review)
+	public ResponseEntity<String> updateReviewById(@PathVariable("reviewId") int reviewId, @RequestBody Review review)
 	{
 	reviewService.updateReviewById(review);
     return ResponseEntity.status(HttpStatus.OK).body("Review is updated....");
