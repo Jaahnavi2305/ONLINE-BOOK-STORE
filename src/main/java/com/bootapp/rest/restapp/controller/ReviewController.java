e8ebf796a5886cc0ad1240ce1908a5d06f68379e
Muppidi <sushma.muppidi@capgemini.com>
2023-02-08 17:12:42 +0530

    commitnew

--- src/main/java/com/bootapp/rest/restapp/controller/ReviewController.java
+++ src/main/java/com/bootapp/rest/restapp/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.bootapp.rest.restapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bootapp.rest.restapp.exception.NullValueException;
 import com.bootapp.rest.restapp.model.Review;
 import com.bootapp.rest.restapp.service.ReviewService;
 
@@ -25,32 +27,50 @@ public class ReviewController {
 
 	/* Review POST API */
 	@PostMapping("/add")
-	public ResponseEntity<String> postReview(@RequestBody Review review) {
-		reviewService.insertReview(review);
+	public ResponseEntity<String> insertReview(@RequestBody Review review) throws NullValueException{
+		reviewService.insertBook(review);
 		return ResponseEntity.status(HttpStatus.OK).body("Review posted in DB");
 	}
 
 	/* Get All API */
 	@GetMapping("/getall")
 	public List<Review> getAllReviews() {
-		List<Review> list = reviewService.getAllReviews();
+		List<Review> list = reviewService.getAllReview();
 		return list;
 	}
 
-	/* Review PUT API */
-	@PutMapping("/update/{reviewId}")
-	public ResponseEntity<String> updateReviewById(@PathVariable("reviewId") int authorId, @RequestBody Review review)
-	{
-	reviewService.updateReviewById(review);
-    return ResponseEntity.status(HttpStatus.OK).body("Review is updated....");
+	/* Review PUT API*/
+	 
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<String> editReview(@PathVariable("id") int id, @RequestBody Review reviewNew) {
+		/* Step 1: check if this id given is valid by fetching the record from DB */
+		Optional<Review> optional = reviewService.getReviewById(id);
+		if (!optional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid ID");
+
+		}
+		Review reviewDB = optional.get();
+		/* Step 2: Set New value to DB value */
+		if (reviewNew.getComments() != null)
+			reviewDB.setComments(reviewNew.getComments());
+		if (reviewNew.getRating() != 0)
+			reviewDB.setRating(reviewNew.getRating());
+		if (reviewNew.getBookName() != null)
+			reviewDB.setBookName(reviewNew.getBookName());
+
+		reviewService.PostReview(reviewDB);
+		return ResponseEntity.status(HttpStatus.OK).body("review record Edited..");
 	}
-	
-	/* Review DELETE API*/
-	@DeleteMapping("/delete/{reviewId}")
-	 public ResponseEntity<String> deleteReviewById(@RequestBody Review review){
-	reviewService.deleteReviewById(review);
-	return ResponseEntity.status(HttpStatus.OK).body("Review is deleted....");
+
+	/* Review DELETE API */
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Object> deleteReviewById(@PathVariable("id") int id) {
+		Optional<Review> optional = reviewService.getReviewById(id);
+		if (!optional.isPresent())
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid ID Given");
+		reviewService.deleteReviewById(id);
+		return ResponseEntity.status(HttpStatus.OK).body("review is deleted");
 	}
 
 }
-
--- src/main/java/com/bootapp/rest/restapp/service/ReviewService.java
+++ src/main/java/com/bootapp/rest/restapp/service/ReviewService.java
@@ -1,34 +1,57 @@
 package com.bootapp.rest.restapp.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bootapp.rest.restapp.data.ReviewRepository;
+import com.bootapp.rest.restapp.exception.NullValueException;
 import com.bootapp.rest.restapp.model.Review;
 
 @Service
 public class ReviewService {
-	
-    @Autowired
-    private ReviewRepository reviewRepository;
-    
-	public void insertReview(Review review) {
-		reviewRepository.save(review);
-	}
 
-	public List<Review> getAllReviews() {
-		return reviewRepository.findAll();
-	}
+	@Autowired
+	private ReviewRepository reviewRepository;
 
-	public void updateReviewById(Review review) {
+	// public void updateReviewById(Review review) {
+	// reviewRepository.save(review);
+	// }
+
+	public void insertBook(Review review) throws NullValueException {
+
+		String s1 = review.getBookName();
+		if (s1 == "") {
+			throw new NullValueException("Enter a bookname first");
+		} else {
 			reviewRepository.save(review);
-			}
-
-	public void deleteReviewById(Review review) {
-		reviewRepository.delete(review);
-	}
-		
+		}
 	}
 
+	public Review deleteReviewById(int id) {
+		Optional<Review> optional = reviewRepository.findById(id);
+		reviewRepository.delete(optional.get());
+		return optional.get();
+	}
 
+	public Optional<Review> getReviewById(int id) {
+		// TODO Auto-generated method stub
+		Optional<Review> optional = reviewRepository.findById(id);
+		return optional;
+	}
+	
+	public List<Review> getAllReview() {
+		List<Review> list = reviewRepository.findAll();
+		return list;
+		}
+
+	public void PostReview(Review reviewDB) {
+		// TODO Auto-generated method stub
+		reviewRepository.save(reviewDB);
+
+	}
+
+	
+}
